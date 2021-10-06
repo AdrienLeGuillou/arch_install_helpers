862d4067566b27f7f018934f309940c9d8ee0441
Adrien Le Guillou <adrien.leguillou@gmail.com>
2021-10-06 15:26:14 +0200

    helpers

---
+++ chroot_helper.sh
@@ -0,0 +1,78 @@
+#!/bin/bash
+
+ln -sf /usr/share/zoneinfo/Europe/Paris /etc/localtime
+hwclock --systohc
+
+echo "en_US.UTF-8 UTF-8" > /etc/locale.gen
+echo "LANG=en_US.UTF-8" > /etc/locale.conf
+
+while true
+do
+    read -p "Enter the hostname for this machine: " NEW_HOSTNAME
+    echo
+
+    echo The new hostname will be:  ${NEW_HOSTNAME}
+    read -p "is this correct? [Y/n] " CORRECT
+
+    case $CORRECT in
+        [yY])
+            break ;;
+        *)
+            echo Starting over
+            ;;
+    esac
+done
+CORRECT=""
+
+echo $NEW_HOSTNAME > /etc/hostname
+echo '127.0.0.1 localhost
+::1\t localhost
+127.0.1.1\t $NEW_HOSTNAME' > /etc/hosts
+
+echo Installing new packages
+
+pacman -S archlinux-keyring
+pacman -S grub networkmanager base-devel git zsh sudo chezmoi
+
+sudo systemctl enable NetworkManager.service
+
+while true
+do
+    read -p "Enter the new user name: " NEW_NAME
+    echo
+
+    echo The new user will be:  ${NEW_NAME}
+    read -p "is this correct? [Y/n] " CORRECT
+
+    case $CORRECT in
+        [yY])
+            break ;;
+        *)
+            echo Starting over
+            ;;
+    esac
+done
+CORRECT=""
+
+useradd -m -G wheel,input,video,audio,storage,disk,kvm  -s /bin/zsh $NEW_NAME
+
+while true
+do
+    echo SUDO configuration, please edit the SUDOER file
+    EDITOR=nvim visudo
+
+    read -p "Is everything correct? [Y/n] " CORRECT
+
+    case $CORRECT in
+        [yY])
+            break ;;
+        *)
+            echo Starting over
+            ;;
+    esac
+done
+CORRECT=""
+
+echo Finished !
+echo Before restarting set the password for root and $NEW_NAME with passwd
+echo and setup grub
---
+++ live_helper.sh
@@ -0,0 +1,44 @@
+#!/bin/bash
+
+echo This script assumes the partions are already mounted on /mnt and the system is connected to the Internet
+read -p "Are you ready to continue? [y/N] " CONTINUE
+case $CONTINUE in
+    [yY])
+        echo
+        ;;
+    *)
+        echo Quitting
+        exit 1
+        ;;
+esac
+
+while true
+do
+    read -p "Enter the swapfile size in Mb: " SWAPSIZE
+    echo
+
+    echo this will create a swapfile of size ${SWAPSIZE}Mb
+    read -p "is this correct? [Y/n] " CORRECT
+
+    case $CORRECT in
+        [yY])
+            break ;;
+        *)
+            echo Starting over
+            ;;
+    esac
+done
+
+timedatectl set-ntp true
+timedatectl status
+
+dd if=/dev/zero of=/mnt/swapfile bs=1M count=$SWAPSIZE status=progress
+chmod 600 /mnt/swapfile
+mkswap /mnt/swapfile
+swapon /mnt/swapfile
+
+pacstrap /mnt base linux linux-firmware neovim
+
+genfstab -U /mnt >> /mnt/etc/fstab
+
+echo Pre chroot phase finished. Run `arch-chroot /mnt` and run the next script
